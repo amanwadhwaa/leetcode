d11ffcdee09d730f816683e8daa9217aa85b7902
amanwadhwaa <amanwadhwa7090@gmail.com>
2026-01-13 20:15:29 +0530

    Time: 2738 ms (5.01%), Space: 48.3 MB (5.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,10 +1,12 @@
 #include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
+        if (s.length() != t.length()) return false;
 
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
@@ -12,17 +14,24 @@ public:
     }
 
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> result;        
-        for(int i = 0 ; i< strs.size() ; i++){
+        vector<vector<string>> result;
+        int n = strs.size();
+        vector<bool> used(n, false);   
+
+        for (int i = 0; i < n; i++) {
+            if (used[i]) continue;    
+
             vector<string> anagrams;
             anagrams.push_back(strs[i]);
-            for(int j= i+1 ; j<strs.size(); j++){
-                if(isAnagram(strs[i],strs[j])){
-                anagrams.push_back(strs[j]);
-                strs.erase(strs.begin() + j);
+            used[i] = true;
 
+            for (int j = i + 1; j < n; j++) {
+                if (!used[j] && isAnagram(strs[i], strs[j])) {
+                    anagrams.push_back(strs[j]);
+                    used[j] = true;    
+                }
             }
-            }
+
             result.push_back(anagrams);
         }
         return result;
