4429b2b42f3b9aa1dabe4172a2a3717f28006194
amanwadhwaa <amanwadhwa7090@gmail.com>
2026-01-16 20:29:56 +0530

    Time: 11 ms (5.1%), Space: 18.3 MB (5.07%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string result="";
+        int leastSize=9999999;
+        for(string s:strs){
+            if(s.size()<leastSize){
+                leastSize=s.size();
+            }
+        }
+        for(int i=0;i<leastSize ;i++){
+            char temp=strs[0][i];
+            for(string s:strs){
+                if(temp==s[i]){
+                    continue;
+                }
+                else{
+                    return result;
+                }
+            }
+            result.push_back(temp);
+        }
+        return result;
+    }
+};
