2b9759b8d8b61e5f4aa72672ecb590b19fcfe40b
Aman Wadhwa <amanwadhwa7090@gmail.com>
2026-03-14 17:27:01 +0530

    Time: 17 ms (5.42%), Space: 27.4 MB (7.84%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    // Helper function to check if a substring (from index l to r) is a palindrome
     bool isPalindrome(string s, int l, int r) {
         while(l < r) {
-            // Skip non-alphanumeric characters on the left
             if(!isalnum(s[l])) {
                 l++;
                 continue;
             }
-            // Skip non-alphanumeric characters on the right
             if(!isalnum(s[r])) {
                 r--;
                 continue;
             }
-            // Compare characters (case-insensitive)
             if(tolower(s[l]) != tolower(s[r])) {
                 return false;
             }
@@ -23,24 +19,21 @@ public:
         return true;
     }
     
-    // Main function to check if string can be palindrome by deleting at most one character
     bool validPalindrome(string s) {
         int l = 0;
         int r = s.size() - 1;
         
         while(l < r) {
-            // Skip non-alphanumeric on left
             if(!isalnum(s[l])) {
                 l++;
                 continue;
             }
-            // Skip non-alphanumeric on right
             if(!isalnum(s[r])) {
                 r--;
                 continue;
             }
             
-            // If characters don't match
+
             if(tolower(s[l]) != tolower(s[r])) {
                 // Try two options:
                 // Option 1: Delete character at position l, check if rest is palindrome
@@ -58,7 +51,6 @@ public:
             r--;
         }
         
-        // If loop completes without mismatch, string is already a palindrome
-        return true;
+           return true;
     }
 };
