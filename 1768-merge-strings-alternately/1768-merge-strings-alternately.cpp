c57c7bf031455b4eb285c4549f3f99a43e0dfca3
Aman Wadhwa <amanwadhwa7090@gmail.com>
2026-03-14 18:09:31 +0530

    Time: 3 ms (40.46%), Space: 8.5 MB (31.98%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int len1  = word1.size();
+        int len2 = word2.size();
+        string result;
+        if(len1 <= len2){
+            for(int i = 0 ; i<len1 ; i++){
+                result.push_back(word1[i]);
+                result.push_back(word2[i]);
+            }
+            for(int i = len1 ; i<len2 ;i++){
+                result.push_back(word2[i]);
+            }
+        }
+        else{
+             for(int i = 0 ; i<len2 ; i++){
+                result.push_back(word1[i]);
+                result.push_back(word2[i]);
+            }
+            for(int i = len2 ; i<len1 ;i++){
+                result.push_back(word1[i]);
+            }
+        }
+        return result;
+    }
+};
