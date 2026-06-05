6749c7c3cae32e709f066b73b26087f37855a07a
Aman Wadhwa <amanwadhwa7090@gmail.com>
2026-06-05 10:29:56 +0530

    Time: 51 ms (1.64%), Space: 39.3 MB (5.28%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -7,17 +7,8 @@ public:
             m[i] = nums[i];
         }
 
-        set<int> s;
-
         for(int i= 0 ; i<nums.size() ; i++){
             int j = (i+k)%nums.size();
-            if(s.size() == nums.size()){
-                break;
-            }
-            if(s.count(i)){
-                continue;
-            }
-            s.insert(i);
             nums[j] = m[i];
         }
         
