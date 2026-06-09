81ae95d70aebd5cf150ce98473194bc8daf06673
Aman Wadhwa <amanwadhwa7090@gmail.com>
2026-06-09 10:16:42 +0530

    Time: 111 ms (5.42%), Space: 92.4 MB (19.85%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,7 +4,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         set<int> s;
         for(int i : nums){
-            if(s.count(i) == 1){
+            if(s.count(i)){
                 return true;
             }
             s.insert(i);
