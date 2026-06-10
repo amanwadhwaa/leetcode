42722610034e5e57e23351190d48d6d11f4a01f6
Aman Wadhwa <amanwadhwa7090@gmail.com>
2026-06-10 16:41:20 +0530

    Time: 17 ms (59.23%), Space: 45.8 MB (36.8%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -6,7 +6,12 @@ public:
         int numBoats=0;
         int r=people.size()-1;
         while(l<=r){
-            if(people[r] + people[l] > limit){
+            if(people[r] == limit){
+                numBoats++;
+                r--;
+                continue;
+            }
+            else if(people[r] + people[l] > limit){
                 numBoats++;
                 r--;
                 continue;
