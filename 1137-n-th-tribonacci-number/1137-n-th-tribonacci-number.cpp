edb34185da52b84b41ea1ea49b86f81b48bd9e58
Aman Wadhwa <amanwadhwa7090@gmail.com>
2026-09-08 22:02:38 +0530

    Time: 0 ms (100%), Space: 8.4 MB (23.86%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    int tri(int n , vector<int>& dp){
-        if(n == 0){
-            return 0;
-        }
-        if(n==1 || n==2){
-            return 1;
-        }
+  int tribonacci(int n) {
+    vector<int> dp(max(3, n+1));
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
 
-        if(dp[n] != -1){
-            return dp[n];
-        }
+    for (int i=3; i<=n; i++)
+      dp[i] = dp[i-3] + dp[i-2] + dp[i-1];
 
-        dp[n] = tri(n-1,dp) + tri(n-2,dp) + tri(n-3,dp);
-
-        return dp[n];
-    }
-    int tribonacci(int n) {
-        vector<int> dp(n+1,-1);
-        return tri(n,dp);
-    }
+    return dp[n];
+  }
 };
